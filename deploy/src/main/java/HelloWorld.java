1cdf67c69d5a68e12bc6b17b38745894465714e5
gouri <gouri@mail.com>
2016-11-07 23:52:14 -0800

    files added

---
+++ deploy/src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by gouri on 11/7/2016.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+}
